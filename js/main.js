b7e3aac7018c8a5db208c9953dec2a267e0594e0
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-11 11:24:22 -0300

    feat: login on kanban page function

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-let subscribeButton = document.getElementById('subscribe-button')
+let subscribeButton = document.getElementById("subscribe-button")
 let email = document.getElementById("subscribe[email]")
 let username = document.getElementById("subscribe[name]")
 
+let loginButton = document.getElementById("login-button")
+let emailLogin = document.getElementById("login[email]")
+let password = document.getElementById("login[password]")
 
-subscribeButton.addEventListener('click', function(e){
-    e.preventDefault()
-    let emailRegex = /\w+@\w+.\w/
-    if(emailRegex.test(email.value)){
-        if(username.value != ""){
-            if(localStorage.getItem(email.value) === null)
-                localStorage.setItem(email.value, username.value)
+if(subscribeButton !== null)
+    subscribeButton.addEventListener('click', function(e){
+        e.preventDefault()
+        let emailRegex = /\w+@\w+.\w/
+        if(emailRegex.test(email.value)){
+            if(username.value != ""){
+                if(localStorage.getItem(email.value) === null)
+                    localStorage.setItem(email.value, username.value)
+            }else
+                alert('Invalid username!')
         }else
-            alert('Invalid username!')
-    }else
-        alert('Invalid email!');
+            alert('Invalid email!');    
+    })
 
-    
+
+loginButton.addEventListener('click', function(e){
+    e.preventDefault()
+    console.log("login")
+    let emailRegex = /\w+@\w+.\w/
+    if(emailRegex.test(emailLogin.value)){
+        if(password.value != "")
+            window.location.href = '/kanban.html'; 
+        else
+            alert('Invalid password!')
+    }else
+        alert('Invalid email!');    
 })
